81c6bca89cb3450d4881c561bd8fbcd153b9f49b
AndreyErmolaev <andrey221985@gmail.com>
2022-06-22 18:52:26 +0700

    stage 2 complete

--- Anti-Fraud System/task/src/antifraud/web/error/UserNotFoundException.java
+++ Anti-Fraud System/task/src/antifraud/web/error/UserNotFoundException.java
@@ -4,6 +4,6 @@ package antifraud.web.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "User not found!")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found!")
 public class UserNotFoundException extends RuntimeException {
 }
